ae4c22d064863d003512922ce59b75237970c86d
zerobits01 <zerobits0101@gmail.com>
2021-06-17 22:04:30 +0430

    overwriting all classes with functions

--- moviesseriesreview/src/components/member/member.js
+++ moviesseriesreview/src/components/member/member.js
@@ -1,34 +1,28 @@
 import React from 'react';
-import {Image} from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './member.css'
 
-class CardMember extends React.Component {
+const CardMember = (props) => {
+    return (
+        <div className="jumbotron col-sm-2 justify-content-center card-member">
+            <center>
 
-    constructor(props){
-        super(props);
-    }
-
-    render(){
-        return (
-            <div className="jumbotron col-sm-2 justify-content-center card-member">
-                <center>
-
-                    <div className="justify-content-center" >
-                        <h4 className="justify-content-center">First Name: {this.props.firstname}</h4><br />
-                        <h4 className="justify-content-center">Last Name: {this.props.lastname}</h4><br />
-                        <p className="text-center">
-                            {this.props.bio}
-                        </p>
-                    </div>
-                    <div className="justify-content-center">
-                        <Image src={this.props.imagesrc} className="person" alt="Random Name" roundedCircle/>
-                    </div>
-                </center>
-            </div>
-        );
-    }
+                <div className="justify-content-center" >
+                    <h4 className="justify-content-center">First Name: {props.firstname}</h4><br />
+                    <h4 className="justify-content-center">Last Name: {props.lastname}</h4><br />
+                    <p className="text-center">
+                        {props.bio}
+                    </p>
+                </div>
+                <div className="justify-content-center">
+                    <Image src={props.imagesrc} className="person" alt="Random Name" roundedCircle />
+                </div>
+            </center>
+        </div>
+    );
 }
+
 
 
 export default CardMember;
--- moviesseriesreview/src/components/testimonials/testimonialitem.js
+++ moviesseriesreview/src/components/testimonials/testimonialitem.js
@@ -2,26 +2,18 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './testimonial.css';
 
-class TestimonialItem extends React.Component {
-
-    constructor(props){
-        super(props)
-    }
-
-    render() {
-        return (
-            <div className="col-lg-4">
-                <div className="testimonial-item mx-auto mb-5 mb-lg-0">
-                    <img className="img-fluid rounded-circle mb-3" src={this.props.imagesrc} alt="" />
-                    <h5>{this.props.name}</h5>
-                    <p className="font-weight-light mb-0">{this.props.comment}</p>
-                </div>
+const TestimonialItem = (props) => {
+    return (
+        <div className="col-lg-4">
+            <div className="testimonial-item mx-auto mb-5 mb-lg-0">
+                <img className="img-fluid rounded-circle mb-3" src={props.imagesrc} alt="" />
+                <h5>{props.name}</h5>
+                <p className="font-weight-light mb-0">{props.comment}</p>
             </div>
+        </div>
 
-        );
-    }
+    );
+
 }
-
-
 
 export default TestimonialItem;
